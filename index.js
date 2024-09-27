041487103d77acf155fa750c30449cb1b085c990
Karimi-F <fridah37karimi@gmail.com>
2024-09-27 13:42:05 +0300

    Completes the scope lab

--- index.js
+++ index.js
@@ -1,1 +1,33 @@
 // Write your solution in this file!
+var customerName = "bob";
+ function customerName (){
+    return customerName
+ }
+
+
+ function upperCaseCustomerName(){
+    customerName = customerName.toUpperCase();
+ }
+
+
+ 
+ function setBestCustomer(){
+    var bestCustomer = 'not bob';
+    window.bestCustomer = bestCustomer;
+   return bestCustomer
+ }
+
+
+ function overwriteBestCustomer(){
+    var bestCustomer = 'maybe bob';
+    window.bestCustomer = bestCustomer;
+ }
+
+ 
+ const leastFavoriteCustomer = 'Ann';
+ function changeLeastFavoriteCustomer(){
+   leastFavoriteCustomer = 'Todd';
+ }
+
+changeLeastFavoriteCustomer();
+
